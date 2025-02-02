e94af44f8a11a0605223fb5bce0fa34256f3cbaf
RMaheshBabu-RVCE24 <maheshbabur.mca24@rvce.edu.in>
2025-02-02 15:09:23 +0530

    variable.sh

---
+++ 03_variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#script to show how to use variables
+
+reg=52
+name="Prateek"
+age=21
+
+echo "My name is : $name having Register number : $reg and my age is : $age"
+
+name="pavan"
+echo "My name is : $name"
