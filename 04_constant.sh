56934bf2fcba17f37360f17d32c3f9f4834704b5
RMaheshBabu-RVCE24 <maheshbabur.mca24@rvce.edu.in>
2025-02-02 15:10:08 +0530

    constant.sh

---
+++ 04_constant.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#this prints the output of a command that may be any commmand like hostname,ls,pwd etc
+echo "host name of the system is : $(hostname)"
+
+#constant variable
+readonly name="Prateek"
+echo "Myname is : $name"
+name="Pavan"
+echo "Myname is : $name"
